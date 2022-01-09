0417304f2a6e2573741cb5bc03bd4368458ffb57
Mislav Petrina <petrinamislav@gmail.com>
2022-01-09 18:31:54 +0100

    ispravak

--- LV10-2D-polja/zadaci/7.zad/main.c
+++ LV10-2D-polja/zadaci/7.zad/main.c
@@ -9,53 +9,61 @@ int main(void) {
 
   int m, i, j, k = 0, h = 0;
 
-  float a[15][15], b[15], c[15], gmax, smin;
-
-  printf("Odredite dimenzije kvadratne matrice <3, 16>: \n");
+  double a[15][15], b[15], c[15], gmax, smin;
+  
+  printf("Odredite dimenzije matrice: \n");
 
   do {
     scanf("%d", &m);
   } while (m <= 3 || m >= 16);
 
-  printf("Popunite matricu: \n");
+  for (i = 0; i < m; i++) {
+    for (j = 0; j < m; j++) {
+      scanf("%lf", &a[i][j]);
+    }
+  }
 
   for (i = 0; i < m; i++) {
     for (j = 0; j < m; j++) {
-      scanf("%f", &a[i][j]);
+      printf("%.2lf ", a[i][j]);
     }
+
+    printf("\n");
   }
 
   for (i = 0; i < m; i++) {
     for (j = 0; j < m; j++) {
       if (i > j) {
-        b[k] = a[i][j];
+        c[k] = a[i][j];
         k++;
       }
+    }
+  }
 
+  gmax = c[0];
+
+  for (i = 0; i < m; i++) {
+    for (j = 0; j < m; j++) {
       if (i + j < m - 1) {
-        c[h] = a[i][j];
+        b[h] = a[i][j];
         h++;
       }
     }
   }
 
-  gmax = b[0];
-  smin = c[0];
+  smin = b[0];
+  
+  for (i = 0; i < m; i++) {
+    if (c[i] > gmax) {
+      gmax = c[i];
+    }
 
-  for (i = 0; i <= k; i++) {
-    if (b[i] > gmax) {
-      gmax = b[i];
+    if (b[i] < smin) {
+      smin = b[i];
     }
   }
 
-  for (i = 0; i <= h; i++) {
-    if (c[i] < smin) {
-      smin = c[i];
-    }
-  }
-
-  printf("Najmanji element iznad sporedne dijagonale: %f\n", smin);
-  printf("Najveci element ispod glavne dijagonale: %f", gmax);
-
+  printf("Najveci ispod glavne: %.2lf\n", gmax);
+  printf("Najmanji iznad sporedne: %.2lf", smin);
   return 0;
 }
